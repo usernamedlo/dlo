ce73843598338035a5e11562b127f263647abf0e
Loïc Ghijselings <l.ghijselings@student.helmo.be>
2023-02-11 13:57:35 +0100

    After 1hour

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, NavLink} from 'react-router-dom';
+import { Home } from './screens/Home';
+import { About } from './components/About';
+import { MyMusics } from './components/MyMusics';
+import { SocialLinks } from './components/SocialLinks';
+import { Contact } from './screens/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink><br></br>
+          <NavLink to="/about">About</NavLink><br></br>
+          <NavLink to="/mymusics">MyMusics</NavLink><br></br>
+          <NavLink to="/sociallinks">SocialLinks</NavLink><br></br>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
       </header>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/mymusics" element={<MyMusics id="AUh9xVoyqvk" />} />
+        <Route path="/mymusics" element={<MyMusics id="5BENMWsG7ks" />} />
+        <Route path="/sociallinks" element={<SocialLinks />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <footer>
+        <p>© 2021 Loïc Ghijselings</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
